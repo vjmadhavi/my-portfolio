781af9413f17bc5f9242b0d49fc40aada4f89a5e
Venu Jakkampudi <venu@Venus-MacBook-Pro.local>
2024-08-14 21:29:33 -0500

    fix bgc

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import NavBar from './NavBar';
+import Footer from './Footer'; 
+import './Page.css';
 import './LandingPage.css'; // Optional: Create and use for styling
 
 const LandingPage = () => {
   return (
     <div className="landing-page">
+      <NavBar />  {/* Add NavBar */}
       <h1>Hello, I'm Madhavi! Welcome to my portfolio</h1>
       <div className="box-container">
         <Link to="/about" className="box">About</Link>
@@ -12,6 +16,7 @@ const LandingPage = () => {
         <Link to="/experience" className="box">Experience</Link>
         <Link to="/education" className="box">Education</Link>
       </div>
+      <Footer /> {/* Add Footer */}
     </div>
   );
 }
